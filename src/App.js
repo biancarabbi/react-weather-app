25cbf9aa4d80d27934e0e5a0d6d17875ae327bd1
Bianca Rabbi <bianca.rabbi1@gmail.com>
2022-07-04 13:47:57 -0300

    added some links

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <hr />
+      <footer>
+        this project was coded by Bianca Rabbi for{" "}
+        <a href="https://www.shecodes.io/">SheCodes.</a>
+        <p>
+          open the repository{" "}
+          <a href="https://github.com/biancarabbi" target="blank">
+            here
+          </a>
+        </p>
+      </footer>
+    </div>
+  );
 }
