ea08443e9295ee372820c35e9f0d6edbc2c20aab
Bianca Rabbi <bianca.rabbi1@gmail.com>
2022-07-04 13:53:31 -0300

    added h1's tag

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
       <hr />
       <footer>
         this project was coded by Bianca Rabbi for{" "}
