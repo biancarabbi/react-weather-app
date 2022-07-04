ea215bb4217108420c5f6925b29421a534a7f54b
Bianca Rabbi <bianca.rabbi1@gmail.com>
2022-07-04 14:44:36 -0300

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,24 +3,26 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <hr />
-      <footer>
-        this project was coded by Bianca Rabbi for{" "}
-        <a href="https://www.shecodes.io/" target="_blank" rel="noreferrer">
-          SheCodes.
-        </a>
-        <p>
-          open the repository{" "}
-          <a
-            href="https://github.com/biancarabbi"
-            target="_blank"
-            rel="noreferrer"
-          >
-            here
+      <div className="container">
+        <h1>Weather App</h1>
+        <hr />
+        <footer>
+          this project was coded by Bianca Rabbi for{" "}
+          <a href="https://www.shecodes.io/" target="_blank" rel="noreferrer">
+            SheCodes.
           </a>
-        </p>
-      </footer>
+          <p>
+            open the repository{" "}
+            <a
+              href="https://github.com/biancarabbi"
+              target="_blank"
+              rel="noreferrer"
+            >
+              here
+            </a>
+          </p>
+        </footer>
+      </div>
     </div>
   );
 }
