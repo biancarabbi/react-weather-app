cf75e4a7151f2ce012f75c9e3d3fb02b6448d823
Bianca Rabbi <bianca.rabbi1@gmail.com>
2022-07-08 15:07:35 -0300

    added some changes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather defaultCity="London" />
+        <Weather defaultCity="São Paulo" />
         <hr />
         <footer>
           this project was coded by Bianca Rabbi for{" "}
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -4,32 +4,34 @@ import WeatherIcon from "./WeatherIcon";
 import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
-  <div className="weather-info">
-    <h1>{props.data.city}</h1>
-    <ul>
-      <li>
-        <FormattedDate date={props.data.date} />
-      </li>
-      <li>{props.data.description}</li>
-    </ul>
+  return (
+    <div className="weather-info">
+      <h1>{props.data.city}</h1>
+      <ul>
+        <li>
+          <FormattedDate date={props.data.date} />
+        </li>
+        <li>{props.data.description}</li>
+      </ul>
 
-    <div className="row mt-3">
-      <div className="col-6">
-        <div className="clearfix">
-          <div className="float-left">
-            <WeatherIcon code={props.data.icon} />
+      <div className="row mt-3">
+        <div className="col-6">
+          <div className="clearfix">
+            <div className="float-left">
+              <WeatherIcon code={props.data.icon} />
+            </div>
+
+            <WeatherTemperature celsius={props.data.temperature} />
           </div>
+        </div>
 
-          <WeatherTemperature celsius={props.data.temperature} />
+        <div className="col-6">
+          <ul>
+            <li>humidity: {props.data.humidity}%</li>
+            <li>wind: {props.data.wind}km/h</li>
+          </ul>
         </div>
       </div>
-
-      <div className="col-6">
-        <ul>
-          <li>humidity: {props.data.humidity}%</li>
-          <li>wind: {props.data.wind}km/h</li>
-        </ul>
-      </div>
     </div>
-  </div>;
+  );
 }
