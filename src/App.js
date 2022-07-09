d94b583e8274bc84b6180d18298519beaa978396
Bianca Rabbi <bianca.rabbi1@gmail.com>
2022-07-09 12:57:41 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ export default function App() {
           <a href="https://www.shecodes.io/" target="_blank" rel="noreferrer">
             SheCodes
           </a>{" "}
-          and hosted on <a href="#">Netlify</a>
+          and hosted on Netlify
           <p>
             open the repository{" "}
             <a
