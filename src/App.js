71b82a46cbc1574c296372fabe188086fe8d6a99
Bianca Rabbi <bianca.rabbi1@gmail.com>
2022-07-09 12:32:19 -0300

    make some changes in the package.json

--- src/App.js
+++ src/App.js
@@ -11,8 +11,9 @@ export default function App() {
         <footer>
           this project was coded by Bianca Rabbi for{" "}
           <a href="https://www.shecodes.io/" target="_blank" rel="noreferrer">
-            SheCodes.
-          </a>
+            SheCodes
+          </a>{" "}
+          and hosted on <a href="">Netlify</a>
           <p>
             open the repository{" "}
             <a
